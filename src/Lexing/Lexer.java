310211243a619513237497509284ccc6ef284872
Edersen Cadeau <edersenc@gmail.com>
2024-02-12 12:21:18 -0500

    Added processSymbol() For two and single char characters. TODO: UnitTest and TouchUps

--- src/Lexing/Lexer.java
+++ src/Lexing/Lexer.java
@@ -10,16 +10,19 @@ public class Lexer {
 
     LinkedList<Token> token = new LinkedList<Token>();
     HashMap<String, Token.TokenType> tokenMap = new HashMap<String, Token.TokenType>();
+    HashMap<String, Token.TokenType> oneCharacter = new HashMap<String, Token.TokenType>();
+    HashMap<String, Token.TokenType> twoCharacter = new HashMap<String, Token.TokenType>();
+    HashMap<String, Token.TokenType> operators = new  HashMap<String, Token.TokenType>();
     public Lexer(String StringHandler,int line, int pos) {
         handler = new CodeHandler(StringHandler, 0);
         this.lineNumber = line;
         this.charPos = pos;
-        populateMap();
+        populateMaps();
 
     }
 
 
-    public void populateMap(){
+    public void populateMaps(){
         tokenMap.put("print", Token.TokenType.PRINT);
         tokenMap.put("read", Token.TokenType.READ);
         tokenMap.put("data", Token.TokenType.DATA);
@@ -34,7 +37,58 @@ public class Lexer {
         tokenMap.put("function", Token.TokenType.Function);
         tokenMap.put("while", Token.TokenType.While);
         tokenMap.put("end", Token.TokenType.END);
+
+
+
+
+        oneCharacter.put("(", Token.TokenType.SINGLE_CHAR_SYMBOL);
+        oneCharacter.put(")", Token.TokenType.SINGLE_CHAR_SYMBOL);
+        oneCharacter.put("<", Token.TokenType.SINGLE_CHAR_SYMBOL);
+        oneCharacter.put(">", Token.TokenType.SINGLE_CHAR_SYMBOL);
+        oneCharacter.put("=", Token.TokenType.SINGLE_CHAR_SYMBOL);
+        oneCharacter.put("+", Token.TokenType.SINGLE_CHAR_SYMBOL);
+        oneCharacter.put("-", Token.TokenType.SINGLE_CHAR_SYMBOL);
+        oneCharacter.put("*", Token.TokenType.SINGLE_CHAR_SYMBOL);
+        oneCharacter.put("/", Token.TokenType.SINGLE_CHAR_SYMBOL);
+
+
+        twoCharacter.put("<>", Token.TokenType.TWO_CHAR_SYMBOL);
+        twoCharacter.put("<=", Token.TokenType.TWO_CHAR_SYMBOL);
+        twoCharacter.put(">=", Token.TokenType.TWO_CHAR_SYMBOL);
+
+
+        operators.put("(", Token.TokenType.LEFT_PAREN);
+        operators.put(")", Token.TokenType.RIGHT_PAREN);
+        operators.put("<", Token.TokenType.LESS_THAN);
+        operators.put("<=", Token.TokenType.LESS_THAN_OR_EQUAL);
+        operators.put(">", Token.TokenType.GREATER_THAN);
+        operators.put(">=", Token.TokenType.GREATER_THAN_OR_EQUAL);
+        operators.put("=", Token.TokenType.EQUALS);
+        operators.put("<>", Token.TokenType.NOT_EQUAL);
+        operators.put("+", Token.TokenType.PLUS);
+        operators.put("-", Token.TokenType.MINUS);
+        operators.put("*", Token.TokenType.MULTIPLY);
+        operators.put("/", Token.TokenType.DIVIDE);
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /**
      * This method is used to process the input string and generate a list of tokens
@@ -82,6 +136,11 @@ public class Lexer {
             //processSymbol();
             HandleStringLiteral();
         }
+            else {
+                // Process a symbol token
+                processSymbol();
+                //System.out.print(processSymbol() + "\n");
+            }
 
         // Move to the next character in the input string
             handler.swallow(1);
@@ -109,9 +168,17 @@ public class Lexer {
         while (Character.isLetter(handler.peek(0))) {
             word.append(handler.getChar());
             pos++;
-            Boolean dollarMod = handler.peek(0)=='$'|| handler.peek(0)=='%';
+            Boolean isLabel = handler.peek(0)==':';
+            Boolean dollarMod = handler.peek(0)=='$'|| handler.peek(0)=='%'
+                    || isLabel;
+
             Boolean carriageReturn = (handler.peek(1)=='\r'|| handler.peek(1)=='\n');
             if (dollarMod && (handler.peek(1)==' '||carriageReturn)){
+                if (isLabel){
+                    token.add(new Token(Token.TokenType.LABEL, lineNumber, charPos, word.toString()));
+                    handler.swallow(1);
+                    charPos++;
+                }
                 word.append(handler.getChar());
                 pos++;
             }
@@ -143,20 +210,19 @@ public class Lexer {
 
     public void HandleStringLiteral(){
         int pos = 0;
+        Boolean escape = false;
         StringBuilder string = new StringBuilder();
         string.append(handler.getChar());
         while (handler.peek(0) != '"'){
-
             if (handler.peek(0) == '\\'){
-
-                for (int i = 0; handler.peek() != '"'; i++){
-
+                handler.swallow(1);
+                while (handler.peek(0) != '\\'){
+                    string.append(handler.getChar());
+                    pos++;
                 }
-                string.append(handler.getChar());
-                pos++;
+                handler.swallow(1);
+
             }
-
-
             string.append(handler.getChar());
             pos++;
         }
@@ -166,6 +232,23 @@ public class Lexer {
         handler.swallow(1);
     }
 
+
+
+    public void processSymbol(){
+        int pos = 0;
+        StringBuilder symbol = new StringBuilder();
+        while (handler.peek(0) != ' '){
+            symbol.append(handler.getChar());
+            pos++;
+        }
+        charPos = charPos + pos;
+        if (oneCharacter.containsKey(symbol.toString())){
+            token.add(new Token(oneCharacter.get(symbol.toString()), lineNumber, charPos, symbol.toString()));
+        } else if (twoCharacter.containsKey(symbol.toString())){
+            token.add(new Token(twoCharacter.get(symbol.toString()), lineNumber, charPos, symbol.toString()));
+        } else
+            token.add(new Token(operators.getOrDefault(symbol.toString(), Token.TokenType.WORD), lineNumber, charPos, symbol.toString()));
+    }
 
 
 
--- src/Lexing/Token.java
+++ src/Lexing/Token.java
@@ -23,7 +23,35 @@ public class Token {
         Function,
         While,
         END,
-        StringLiteral
+        StringLiteral,
+
+        SINGLE_CHAR_SYMBOL,
+
+        TWO_CHAR_SYMBOL,
+
+        RIGHT_PAREN,
+
+        LEFT_PAREN,
+
+        LESS_THAN,
+        LESS_THAN_OR_EQUAL,
+        GREATER_THAN,
+        GREATER_THAN_OR_EQUAL,
+        EQUALS,
+
+        NOT_EQUAL,
+        PLUS,
+
+        MINUS,
+
+        MULTIPLY,
+
+        DIVIDE,
+        LABEL,
+
+
+
+
 
     }
 
