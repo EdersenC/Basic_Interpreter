d7b52b2fbb389e67799d0f366682908f9761572e
Eddy <edersenc@gmail.com>
2024-02-17 15:32:57 -0500

    Started Parser

--- src/Parsing/Parser.java
+++ src/Parsing/Parser.java
@@ -4,6 +4,7 @@ import Lexing.Token;
 import Parsing.Nodes.ProgramNode;
 
 import java.util.LinkedList;
+import java.util.Optional;
 
 public class Parser {
 
@@ -14,8 +15,11 @@ public class Parser {
     }
 
 
-
-    public ProgramNode parse(String input){
+    /**
+     *
+     * @return the parsed program node
+     */
+    public ProgramNode parse(){
         while (handler.moreTokens()){
             handler.peek(0);
         }
@@ -26,6 +30,26 @@ public class Parser {
 
 
 
+    /**
+     * @return the expression
+     */
+    public Optional<Token> expression(){
+        return handler.peek(0);
+    }
+
+    /**
+     * @return the term
+     */
+    public Optional<Token> term(){
+        return handler.peek(0);
+    }
+
+    /**
+     * @return the factor
+     */
+    public Optional<Token> factor(){
+        return handler.peek(0);
+    }
 
 
 
